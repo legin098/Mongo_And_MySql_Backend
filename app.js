45d0551d160bd378d54b1b1b8a3b550960851c0e
Jesus Cruz <jesus.cruz@campusvirtual.aunarvillavicencio.edu.co>
2023-02-03 20:04:23 -0500

    first commit :construction:

---
+++ app.js
@@ -0,0 +1,12 @@
+require("dotenv").config()
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`The service is runnig on http://localhost:${port}`);
+});
