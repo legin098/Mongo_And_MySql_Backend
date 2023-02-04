5d4e9e420bc0b0e08187041c35c8971ce55d89d8
Jesus Cruz <jesus.cruz@campusvirtual.aunarvillavicencio.edu.co>
2023-02-04 12:03:32 -0500

    data validation :white_check_mark:

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { getItems, createItem } = require("../controllers/tracks");
+const { validatorCreateItem } = require("../validators/tracks");
 const router = express.Router();
 
 //TODO: http://localhost:3001/api/tracks GET, POST, PUT, DELETE
 
 router.get("/", getItems);
-router.post("/", createItem);
+router.post("/", validatorCreateItem, createItem);
 
 module.exports = router;
