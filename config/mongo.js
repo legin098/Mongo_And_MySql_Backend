9a4956ac6e102fd20c40524d2dd38a1c6df0506b
Jesus Cruz <jesus.cruz@campusvirtual.aunarvillavicencio.edu.co>
2023-02-03 20:27:25 -0500

    Scaffold :building_construction: and mongoDb connect :white_check_mark:

---
+++ config/mongo.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const dbConnect = () => {
+  mongoose.set("strictQuery", false);
+  const DB_URI = process.env.DB_URI;
+  mongoose.connect(
+    DB_URI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    (err, res) => {
+      if (!err) {
+        console.log("*** CORRECT CONNECTION ***");
+      } else {
+        console.log("*** CONNECTION ERROR ***");
+      }
+    }
+  );
+};
+
+module.exports = dbConnect;
